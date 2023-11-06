ce0c3a54a52f64fc78d8e5e7ede2042845e2e2d5
John Yenter-Briars <jyenterbriars@gmail.com>
2023-11-06 10:16:31 -0600

    remove large config dirs that are unused

--- pull_files.sh
+++ pull_files.sh
@@ -7,7 +7,6 @@ cp ~/.Xresources .
 cp ~/.Xresources-hdmi .
 cp ~/.xinitrc .
 cp ~/.xprofile .
-cp -r ~/.vscode .
 
 cp -r  ~/.local/bin/* .local/bin/
 
@@ -22,6 +21,11 @@ code --list-extensions > vscode/extensions.txt
 #to install: --install-extension <ext> see 'code --help'
 cp ~/.config/Code/User/keybindings.json vscode/keybindings.json
 cp ~/.config/Code/User/settings.json vscode/settings.json
+
+rm -r .config/Code
+rm -r .config/google-chrome
+rm -r .config/discord
+yes | rm -r .vscode/
 
 crontab -l > ./cron/currentcrontab
 
